bcaf72ac6baf4b4fafcb878a74fb4ce7ff2cacb2
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-11-22 10:00:49 +0800

    system/program.go: add CommandLineToArgv() and TestCommandLineToArgv().

--- internal/system/program_test.go
+++ internal/system/program_test.go
@@ -83,3 +83,29 @@ func TestPrintError(t *testing.T) {
 
 	PrintError("test error")
 }
+
+func TestCommandLineToArgv(t *testing.T) {
+	exe1 := "test"
+	exe2 := `"test test"`
+	for _, testdata := range [...]*struct {
+		cmd  string
+		args []string
+	}{
+		{"net", []string{"net"}},
+		{`net -a -b`, []string{"net", "-a", "-b"}},
+		{`net -a -b "a"`, []string{"net", "-a", "-b", "a"}},
+		{`"net net"`, []string{"net net"}},
+		{`"net\net"`, []string{`net\net`}},
+		{`"net\net net"`, []string{`net\net net`}},
+		{`net -a \"net`, []string{"net", "-a", `"net`}},
+		{`net -a ""`, []string{"net", "-a", ""}},
+		{`""net""  -a  -b`, []string{"net", "-a", "-b"}},
+		{`"""net""" -a`, []string{`"net"`, "-a"}},
+	} {
+		args := CommandLineToArgv(exe1 + " " + testdata.cmd)
+		require.Equal(t, append([]string{"test"}, testdata.args...), args)
+
+		args = CommandLineToArgv(exe2 + " " + testdata.cmd)
+		require.Equal(t, append([]string{"test test"}, testdata.args...), args)
+	}
+}
