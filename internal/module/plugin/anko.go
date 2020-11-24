d974ad9438d5a9d400e8b0eff585643cd4d5be03
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-11-24 19:28:43 +0800

    module/plugin/anko.go: add NewAnko(), check load module and define external.

--- internal/module/plugin/anko.go
+++ internal/module/plugin/anko.go
@@ -2,46 +2,92 @@ package plugin
 
 import (
 	"context"
+	"fmt"
+	"time"
 
-	"project/external/anko/ast"
-	"project/internal/module"
+	"github.com/pkg/errors"
 
 	"project/internal/anko"
 )
 
 type Anko struct {
-	stmt ast.Stmt // store parsed anko script
+	env *anko.Env
 
-	ctx    context.Context
-	cancel context.CancelFunc
+	// in script
+	start   func() error
+	stop    func()
+	restart func() error
+	name    func() string
+	info    func() string
+	status  func() string
 }
 
-func NewAnko(script string) (module.Module, error) {
-	anko.NewEnv()
+// NewAnko is used to create a custom module from anko script.
+func NewAnko(ctx context.Context, external interface{}, script string) (*Anko, error) {
+	ast, err := anko.ParseSrc(script)
+	if err != nil {
+		return nil, err
+	}
+	env := anko.NewEnv()
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	ret, err := anko.RunContext(ctx, env, ast)
+	if err != nil {
+		return nil, err
+	}
+	// check is load successfully
+	switch ret := ret.(type) {
+	case bool:
+		if !ret {
+			return nil, errors.New("return value is false")
+		}
+	default:
+		return nil, errors.Errorf("unexcepted return value: %s", ret)
+	}
+	// define external
+	ext, err := env.Get("external")
+	if err == nil {
+		return nil, errors.Errorf("already define external: %v", ext)
+	}
+	err = env.Define("external", external)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to define external")
+	}
+
+	// get exported function
+	start, err := env.Get("Start")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get Start function")
+	}
+	startFn, ok := start.(func() error)
+	if ok {
+		return nil, errors.Wrap(err, "invalid Start function")
+	}
+	fmt.Println(startFn())
 
 	return nil, nil
 }
 
-func (m *Anko) Start() error {
-	return nil
+func (ank *Anko) Start() error {
+	return ank.start()
 }
 
-func (m *Anko) Stop() {
-
+func (ank *Anko) Stop() {
+	ank.stop()
 }
 
-func (m *Anko) Restart() error {
-	return nil
+func (ank *Anko) Restart() error {
+	return ank.restart()
 }
 
-func (m *Anko) Name() string {
-	return ""
+func (ank *Anko) Name() string {
+	return ank.name()
 }
 
-func (m *Anko) Info() string {
-	return ""
+func (ank *Anko) Info() string {
+	return ank.info()
 }
 
-func (m *Anko) Status() string {
-	return ""
+func (ank *Anko) Status() string {
+	return ank.status()
 }
--- internal/module/plugin/plugin.go
+++ internal/module/plugin/plugin.go
@@ -1,6 +1,8 @@
 package plugin
 
 import (
+	"context"
+
 	"github.com/pkg/errors"
 
 	"project/internal/module"
@@ -12,12 +14,14 @@ const (
 	ModeYaegi = "yaegi" // support go 1.13
 )
 
-// New is used to create a new plugin.
+// New is used to create a new plugin from script.
+// external include role functions like Sender.Send(),
+// the script can use external to call Role self function.
 // [warning]: script string will covered after call.
-func New(mode string, script string) (module.Module, error) {
+func New(ctx context.Context, external interface{}, mode, script string) (module.Module, error) {
 	switch mode {
 	case ModeAnko:
-		return nil, nil
+		return NewAnko(ctx, external, script)
 	case ModeYaegi:
 		return nil, nil
 	default:
