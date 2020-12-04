a02540ad057d0030acda69671649df6adb030aad
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-12-04 20:04:47 +0800

    install: add patch/verify.go, add verifyPatchFiles(), update switch in main().

--- script/install/install.go
+++ script/install/install.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/pkg/errors"
+
 	"project/internal/logger"
 	"project/internal/system"
 
@@ -25,30 +27,33 @@ var showModules bool
 
 func main() {
 	var (
-		path           string
+		configPath     string
 		installPatch   bool
 		uninstallPatch bool
+		verifyPatch    bool
 	)
-	flag.StringVar(&path, "config", "config.json", "configuration file path")
+	flag.StringVar(&configPath, "config", "config.json", "configuration file path")
 	flag.BoolVar(&showModules, "show-modules", false, "show modules when list module")
 	flag.BoolVar(&installPatch, "install-patch", false, "only install patch files")
 	flag.BoolVar(&uninstallPatch, "uninstall-patch", false, "only uninstall patch files")
+	flag.BoolVar(&verifyPatch, "verify-patch", false, "only verify patch files")
 	flag.Parse()
-	if !config.Load(path, &cfg) {
+	if !config.Load(configPath, &cfg) {
 		return
 	}
 	switch {
 	case installPatch:
-		if installPatchFiles() {
-			log.Println(logger.Info, "install patch files successfully")
-		}
-		return
+		installPatchFiles()
 	case uninstallPatch:
-		if uninstallPatchFiles() {
-			log.Println(logger.Info, "uninstall patch files successfully")
-		}
-		return
+		uninstallPatchFiles()
+	case verifyPatch:
+		verifyPatchFiles()
+	default:
+		installDefault()
 	}
+}
+
+func installDefault() {
 	for _, step := range [...]func() bool{
 		installPatchFiles,
 		listModule,
@@ -84,6 +89,7 @@ func getGoRootPaths() []string {
 }
 
 func installPatchFiles() bool {
+	log.Println(logger.Info, "install patch files")
 	paths := getGoRootPaths()
 	var errs []error
 	walkFunc := func(path string, stat os.FileInfo, err error) error {
@@ -115,7 +121,7 @@ func installPatchFiles() bool {
 		return false
 	}
 	if len(errs) == 0 {
-		log.Println(logger.Info, "install all patch files to go root path")
+		log.Println(logger.Info, "install patch files successfully")
 		return true
 	}
 	log.Println(logger.Error, "appear error when install patch file")
@@ -134,6 +140,7 @@ func copyFileToGoRoot(src, dst string) error {
 }
 
 func uninstallPatchFiles() bool {
+	log.Println(logger.Info, "uninstall patch files")
 	paths := getGoRootPaths()
 	var errs []error
 	walkFunc := func(path string, stat os.FileInfo, err error) error {
@@ -165,7 +172,7 @@ func uninstallPatchFiles() bool {
 		return false
 	}
 	if len(errs) == 0 {
-		log.Println(logger.Info, "uninstall all patch files in go root path")
+		log.Println(logger.Info, "uninstall patch files successfully")
 		return true
 	}
 	log.Println(logger.Error, "appear error when uninstall patch file")
@@ -173,6 +180,53 @@ func uninstallPatchFiles() bool {
 		log.Println(logger.Error, errs[i])
 	}
 	return false
+}
+
+func verifyPatchFiles() bool {
+	log.Println(logger.Info, "verify patch files")
+	list := []string{
+		cfg.Common.GoRootLatest,
+		cfg.Common.GoRoot1108,
+		cfg.Common.GoRoot11113,
+		cfg.Common.GoRoot11217,
+		cfg.Common.GoRoot11315,
+		cfg.Common.GoRoot11415,
+		cfg.Common.GoRoot115x,
+	}
+	paths := make([]string, 0, len(list))
+	for i := 0; i < len(list); i++ {
+		if list[i] == "" {
+			continue
+		}
+		paths = append(paths, list[i]+"/bin/go")
+	}
+	pathsLen := len(paths)
+	errCh := make(chan error, pathsLen)
+	for _, path := range paths {
+		go func(path string) {
+			var err error
+			defer func() { errCh <- err }()
+			output, code, err := exec.Run(path, "run", "script/install/patch/verify.go")
+			if err != nil {
+				return
+			}
+			output = output[:len(output)-1] // remove the last "\n"
+			if code != 0 {
+				err = errors.New(output)
+				return
+			}
+			log.Println(logger.Info, "go run output:\n"+output)
+		}(path)
+	}
+	for i := 0; i < pathsLen; i++ {
+		err := <-errCh
+		if err != nil {
+			log.Println(logger.Error, err)
+			return false
+		}
+	}
+	log.Println(logger.Info, "verify patch files successfully")
+	return true
 }
 
 func listModule() bool {
@@ -215,6 +269,7 @@ func downloadAllModules() bool {
 }
 
 func verifyModule() bool {
+	log.Println(logger.Info, "verify modules")
 	output, code, err := exec.Run("go", "mod", "verify")
 	if err != nil {
 		log.Println(logger.Error, err)
---
+++ script/install/patch/verify.go
@@ -0,0 +1,107 @@
+// this package is used to test patch files can build pass.
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/textproto"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
+)
+
+func main() {
+	fmt.Println("--------------------------------")
+	fmt.Println("go version:", runtime.Version())
+	fmt.Println("--------------------------------")
+
+	// bytes
+	buf := bytes.NewBuffer(nil)
+	bytes.ReplaceAll(nil, nil, nil)
+	bytes.ToValidUTF8(nil, nil)
+
+	// crypto/ed25519
+	fmt.Println(ed25519.GenerateKey(rand.Reader))
+
+	// crypto/x509
+	x509.NewCertPool().Certs()
+
+	// encoding/json
+	json.NewDecoder(buf).InputOffset()
+
+	// errors
+	errors.Is(nil, nil)
+
+	// io
+	var sw io.StringWriter
+	fmt.Println(sw)
+	fmt.Println(io.Discard)
+	fmt.Println(io.NopCloser(nil))
+	fmt.Println(io.ReadAll(buf))
+
+	// log
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	fmt.Println(logger.Writer())
+	fmt.Println(log.Writer())
+	fmt.Println(log.Default())
+
+	// net
+	fmt.Println(net.ErrClosed)
+
+	// net/http
+	httpClient := http.Client{
+		Transport: new(http.Transport),
+	}
+	httpClient.CloseIdleConnections()
+	httpHeader := make(http.Header)
+	httpHeader.Clone()
+	httpHeader.Values("test")
+	ctx := context.Background()
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://test.com/", nil)
+	req.Clone(ctx)
+	new(http.Transport).Clone()
+
+	// net/textproto
+	textproto.MIMEHeader(httpHeader).Values("test")
+
+	// os
+	new(os.ProcessState).ExitCode()
+	fmt.Println(new(os.File).SyscallConn())
+	fmt.Println(os.UserCacheDir())
+	fmt.Println(os.UserHomeDir())
+	fmt.Println(os.UserConfigDir())
+
+	fmt.Println(os.ReadDir("test"))
+	fmt.Println(os.CreateTemp("", ""))
+	fmt.Println(os.MkdirTemp("", ""))
+
+	// reflect
+	fmt.Println(reflect.Value{})
+
+	runtime.GC()
+
+	// strings
+	new(strings.Builder).Len()
+	strings.ReplaceAll("", "", "")
+	strings.ToValidUTF8("", "")
+
+	time.Duration(111).Microseconds()
+	time.Duration(111).Milliseconds()
+
+	fmt.Println("--------------------------------")
+	fmt.Println("pass!")
+	fmt.Println("--------------------------------")
+}
