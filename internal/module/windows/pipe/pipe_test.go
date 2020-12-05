b7b7a7d54759f48b50de44815b04e7cbcf6e18c8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-12-05 09:10:01 +0800

    module/windows/pipe/pipe_test.go: finish TestPipe(), TestListen() and TestDial().

--- internal/module/windows/pipe/pipe_test.go
+++ internal/module/windows/pipe/pipe_test.go
@@ -3,31 +3,120 @@
 package pipe
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"syscall"
 	"testing"
 
+	"github.com/Microsoft/go-winio"
 	"github.com/stretchr/testify/require"
 
 	"project/internal/patch/toml"
 	"project/internal/testsuite"
 )
 
-const testPipePath = `\\.\pipe\test`
+// create leaks goroutine first in github.com/Microsoft/go-winio.
+func init() {
+	_, _ = winio.MakeOpenFile(0)
+}
+
+const (
+	testPipePath = `\\.\pipe\test`
+	testMessage  = "hello"
+)
 
 func TestPipe(t *testing.T) {
-	fmt.Println(ErrListenerClosed)
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
 
 	listener, err := Listen(testPipePath, nil)
 	require.NoError(t, err)
-	defer func() {
-		err := listener.Close()
-		require.NoError(t, err)
+	defer func() { _ = listener.Close() }()
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+			_, err = conn.Write([]byte(testMessage))
+			require.NoError(t, err)
+			err = conn.Close()
+			require.NoError(t, err)
+		}
 	}()
+
+	conn, err := Dial(testPipePath, nil)
+	require.NoError(t, err)
+	buf := make([]byte, len(testMessage))
+	_, err = io.ReadFull(conn, buf)
+	require.NoError(t, err)
+	require.Equal(t, testMessage, string(buf))
+	err = conn.Close()
+	require.NoError(t, err)
+
+	err = listener.Close()
+	require.NoError(t, err)
+
+	fmt.Println(ErrListenerClosed)
 }
 
 func TestListen(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
 
+	cfg := Config{MessageMode: true}
+	listener, err := Listen(testPipePath, &cfg)
+	require.NoError(t, err)
+
+	err = listener.Close()
+	require.NoError(t, err)
+}
+
+func TestDial(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
+
+	listener, err := Listen(testPipePath, nil)
+	require.NoError(t, err)
+	defer func() { _ = listener.Close() }()
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+			_, err = conn.Write([]byte(testMessage))
+			require.NoError(t, err)
+			err = conn.Close()
+			require.NoError(t, err)
+		}
+	}()
+
+	// DialContext
+	conn, err := DialContext(context.Background(), testPipePath)
+	require.NoError(t, err)
+	buf := make([]byte, len(testMessage))
+	_, err = io.ReadFull(conn, buf)
+	require.NoError(t, err)
+	require.Equal(t, testMessage, string(buf))
+	err = conn.Close()
+	require.NoError(t, err)
+
+	// DialAccess
+	const access = syscall.GENERIC_READ | syscall.GENERIC_WRITE
+	conn, err = DialAccess(context.Background(), testPipePath, access)
+	require.NoError(t, err)
+	buf = make([]byte, len(testMessage))
+	_, err = io.ReadFull(conn, buf)
+	require.NoError(t, err)
+	require.Equal(t, testMessage, string(buf))
+	err = conn.Close()
+	require.NoError(t, err)
+
+	err = listener.Close()
+	require.NoError(t, err)
 }
 
 func TestConfig(t *testing.T) {
