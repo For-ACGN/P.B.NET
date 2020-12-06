2be871f37d11765565d6da00d9d748c7f32c9c29
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-12-06 09:35:54 +0800

    nmap/options: update fields type about timeout, add TestOptions_ToArgs() and TestOptions().

--- scanner/nmap/options.go
+++ scanner/nmap/options.go
@@ -3,7 +3,6 @@ package nmap
 import (
 	"strconv"
 	"strings"
-	"time"
 
 	"project/internal/system"
 )
@@ -13,7 +12,9 @@ type Options struct {
 	// ---------------------------------basic scan---------------------------------
 	// ScanTech is use specified scan technique.
 	// TCP Scan default technique is -sS, TCP SYN.
-	// UDP Scan will be set automatically if Job.Protocol is "udp" or "all".
+	// UDP Scan will be set automatically if Job.Protocol is "udp",
+	// don't set it again.
+	// Must not add "-" before it, we added it.
 	//
 	// -sS/sT/sA/sW/sM: TCP SYN/Connect()/ACK/Window/Maimon scans
 	// -sU: UDP Scan
@@ -53,11 +54,11 @@ type Options struct {
 
 	// HostTimeout is the nmap argument "--host-timeout", it is used to
 	// Give up on target after this long.
-	HostTimeout time.Duration `toml:"host_timeout" json:"host_timeout"`
+	HostTimeout string `toml:"host_timeout" json:"host_timeout"`
 
 	// MaxRTTTimeout is the nmap argument "--max-rtt-timeout", it is
 	// used to specifies probe round trip time.
-	MaxRTTTimeout time.Duration `toml:"max_rtt_timeout" json:"max_rtt_timeout"`
+	MaxRTTTimeout string `toml:"max_rtt_timeout" json:"max_rtt_timeout"`
 
 	// MinRate is the nmap argument "--min-rate", send packets no slower
 	// than <number> per second.
@@ -79,7 +80,7 @@ func (opts *Options) ToArgs() []string {
 	args := make([]string, 0, 8)
 	// ---------------------------------basic scan---------------------------------
 	if opts.ScanTech != "" {
-		args = append(args, opts.ScanTech)
+		args = append(args, "-"+opts.ScanTech)
 	}
 	if opts.NoPing {
 		args = append(args, "-Pn")
@@ -98,11 +99,11 @@ func (opts *Options) ToArgs() []string {
 		args = append(args, "-S", opts.LocalIP[0])
 	}
 	// --------------------------------performance---------------------------------
-	if opts.HostTimeout > 0 {
-		args = append(args, "--host-timeout", opts.HostTimeout.String())
+	if opts.HostTimeout != "" {
+		args = append(args, "--host-timeout", opts.HostTimeout)
 	}
-	if opts.MaxRTTTimeout > 0 {
-		args = append(args, "--max-rtt-timeout", opts.MaxRTTTimeout.String())
+	if opts.MaxRTTTimeout != "" {
+		args = append(args, "--max-rtt-timeout", opts.MaxRTTTimeout)
 	}
 	if opts.MinRate > 0 {
 		args = append(args, "--min-rate", strconv.Itoa(opts.MinRate))
--- scanner/nmap/options_test.go
+++ scanner/nmap/options_test.go
@@ -1,1 +1,63 @@
 package nmap
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/patch/toml"
+	"project/internal/testsuite"
+)
+
+func TestOptions_ToArgs(t *testing.T) {
+	opts := Options{
+		ScanTech:      "sS",
+		NoPing:        true,
+		Service:       true,
+		OS:            true,
+		Device:        "eth0",
+		LocalIP:       []string{"192.168.1.2"},
+		HostTimeout:   "10m",
+		MaxRTTTimeout: "50ms",
+		MinRate:       1000,
+		MaxRate:       10000,
+		Arguments:     "--ttl 128 --badsum",
+	}
+	const cmd = "-sS -Pn -sV -O -e eth0 -S 192.168.1.2 " +
+		"--host-timeout 10m --max-rtt-timeout 50ms " +
+		"--min-rate 1000 --max-rate 10000 --ttl 128 --badsum"
+	require.Equal(t, cmd, opts.String())
+}
+
+func TestOptions(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/options.toml")
+	require.NoError(t, err)
+
+	// check unnecessary field
+	opts := Options{}
+	err = toml.Unmarshal(data, &opts)
+	require.NoError(t, err)
+
+	// check zero value
+	testsuite.ContainZeroValue(t, opts)
+
+	for _, testdata := range [...]*struct {
+		expected interface{}
+		actual   interface{}
+	}{
+		{expected: "sS", actual: opts.ScanTech},
+		{expected: true, actual: opts.NoPing},
+		{expected: true, actual: opts.Service},
+		{expected: true, actual: opts.OS},
+		{expected: "eth0", actual: opts.Device},
+		{expected: []string{"192.168.1.2"}, actual: opts.LocalIP},
+		{expected: "10m", actual: opts.HostTimeout},
+		{expected: "50ms", actual: opts.MaxRTTTimeout},
+		{expected: 1000, actual: opts.MinRate},
+		{expected: 10000, actual: opts.MaxRate},
+		{expected: "--ttl 128 --badsum", actual: opts.Arguments},
+	} {
+		require.Equal(t, testdata.expected, testdata.actual)
+	}
+}
