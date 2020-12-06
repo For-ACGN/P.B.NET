52a3b6931059952c158cbaa1933ced9962248c54
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-12-06 13:39:40 +0800

    nmap/output.go: add more structures about output, update some comments.

--- scanner/nmap/output.go
+++ scanner/nmap/output.go
@@ -109,3 +109,132 @@ type Target struct {
 	Status        string `xml:"status,attr" json:"status"`
 	Reason        string `xml:"reason,attr" json:"reason"`
 }
+
+// Host contains all information about a single host.
+type Host struct {
+	StartTime     Timestamp     `xml:"starttime,attr" json:"starttime"`
+	EndTime       Timestamp     `xml:"endtime,attr" json:"endtime"`
+	Comment       string        `xml:"comment,attr" json:"comment"`
+	Status        Status        `xml:"status" json:"status"`
+	Addresses     []Address     `xml:"address" json:"addresses"`
+	Hostnames     []Hostname    `xml:"hostnames>hostname" json:"hostnames"`
+	Smurfs        []Smurf       `xml:"smurf" json:"smurfs"`
+	Ports         []Port        `xml:"ports>port" json:"ports"`
+	ExtraPorts    []ExtraPorts  `xml:"ports>extraports" json:"extraports"`
+	Os            Os            `xml:"os" json:"os"`
+	Distance      Distance      `xml:"distance" json:"distance"`
+	Uptime        Uptime        `xml:"uptime" json:"uptime"`
+	TcpSequence   TcpSequence   `xml:"tcpsequence" json:"tcpsequence"`
+	IpIdSequence  IpIdSequence  `xml:"ipidsequence" json:"ipidsequence"`
+	TcpTsSequence TcpTsSequence `xml:"tcptssequence" json:"tcptssequence"`
+	HostScripts   []Script      `xml:"hostscript>script" json:"hostscripts"`
+	Trace         Trace         `xml:"trace" json:"trace"`
+	Times         Times         `xml:"times" json:"times"`
+}
+
+// Status is the host's status. Up, down, etc.
+type Status struct {
+	State     string  `xml:"state,attr" json:"state"`
+	Reason    string  `xml:"reason,attr" json:"reason"`
+	ReasonTTL float32 `xml:"reason_ttl,attr" json:"reason_ttl"`
+}
+
+// Address contains a IPv4 or IPv6 address for a Host.
+type Address struct {
+	Addr     string `xml:"addr,attr" json:"addr"`
+	AddrType string `xml:"addrtype,attr" json:"addrtype"`
+	Vendor   string `xml:"vendor,attr" json:"vendor"`
+}
+
+// Hostname is a single name for a Host.
+type Hostname struct {
+	Name string `xml:"name,attr" json:"name"`
+	Type string `xml:"type,attr" json:"type"`
+}
+
+// Smurf contains repsonses from a smurf attack. I think. Smurf attacks, really?
+type Smurf struct {
+	Responses string `xml:"responses,attr" json:"responses"`
+}
+
+// ExtraPorts contains the information about the closed|filtered ports.
+type ExtraPorts struct {
+	State   string   `xml:"state,attr" json:"state"`
+	Count   int      `xml:"count,attr" json:"count"`
+	Reasons []Reason `xml:"extrareasons" json:"reasons"`
+}
+
+// Reason is the ExtraPorts reason.
+type Reason struct {
+	Reason string `xml:"reason,attr" json:"reason"`
+	Count  int    `xml:"count,attr" json:"count"`
+}
+
+// Port contains all the information about a scanned port.
+type Port struct {
+	Protocol string   `xml:"protocol,attr" json:"protocol"`
+	PortId   int      `xml:"portid,attr" json:"id"`
+	State    State    `xml:"state" json:"state"`
+	Owner    Owner    `xml:"owner" json:"owner"`
+	Service  Service  `xml:"service" json:"service"`
+	Scripts  []Script `xml:"script" json:"scripts"`
+}
+
+// State contains information about a given ports status.
+// State will be open, closed, etc.
+type State struct {
+	State     string  `xml:"state,attr" json:"state"`
+	Reason    string  `xml:"reason,attr" json:"reason"`
+	ReasonTTL float32 `xml:"reason_ttl,attr" json:"reason_ttl"`
+	ReasonIP  string  `xml:"reason_ip,attr" json:"reason_ip"`
+}
+
+// Owner contains the name of Port.Owner.
+type Owner struct {
+	Name string `xml:"name,attr" json:"name"`
+}
+
+// Service contains detailed information about a Port's service details.
+type Service struct {
+	Name       string `xml:"name,attr" json:"name"`
+	Conf       int    `xml:"conf,attr" json:"conf"`
+	Method     string `xml:"method,attr" json:"method"`
+	Version    string `xml:"version,attr" json:"version"`
+	Product    string `xml:"product,attr" json:"product"`
+	ExtraInfo  string `xml:"extrainfo,attr" json:"extrainfo"`
+	Tunnel     string `xml:"tunnel,attr" json:"tunnel"`
+	Proto      string `xml:"proto,attr" json:"proto"`
+	Rpcnum     string `xml:"rpcnum,attr" json:"rpcnum"`
+	Lowver     string `xml:"lowver,attr" json:"lowver"`
+	Highver    string `xml:"hiver,attr" json:"hiver"`
+	Hostname   string `xml:"hostname,attr" json:"hostname"`
+	OsType     string `xml:"ostype,attr" json:"ostype"`
+	DeviceType string `xml:"devicetype,attr" json:"devicetype"`
+	ServiceFp  string `xml:"servicefp,attr" json:"servicefp"`
+	CPEs       []CPE  `xml:"cpe" json:"cpes"`
+}
+
+// CPE (Common Platform Enumeration) is a standardized way to name
+// software applications, operating systems, and hardware platforms.
+type CPE string
+
+// Script contains information from Nmap Scripting Engine.
+type Script struct {
+	Id       string    `xml:"id,attr" json:"id"`
+	Output   string    `xml:"output,attr" json:"output"`
+	Tables   []Table   `xml:"table" json:"tables"`
+	Elements []Element `xml:"elem" json:"elements"`
+}
+
+// Table contains the output of the script in a more parse-able form.
+type Table struct {
+	Key      string    `xml:"key,attr" json:"key"`
+	Elements []Element `xml:"elem" json:"elements"`
+	Table    []Table   `xml:"table" json:"tables"`
+}
+
+// Element contains the output of the script, with detailed information
+type Element struct {
+	Key   string `xml:"key,attr" json:"key"`
+	Value string `xml:",chardata" json:"value"`
+}
