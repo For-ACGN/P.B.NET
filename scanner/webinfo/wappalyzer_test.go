b5d92e776c530e56f5a27242d56dfd456f7f5719
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-12-10 20:01:04 +0800

    webinfo: add cms testdata and testDownloadWappalyzerData() to download data.

--- scanner/webinfo/wappalyzer_test.go
+++ scanner/webinfo/wappalyzer_test.go
@@ -1,1 +1,32 @@
 package webinfo
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"project/internal/system"
+	"project/internal/testsuite"
+)
+
+const testTechFilePath = "testdata/tech.json"
+
+// download tech.json file if it is not exist in the testdata directory.
+func testDownloadWappalyzerData() {
+	exist, err := system.IsExist(testTechFilePath)
+	testsuite.TestMainCheckError(err)
+	if exist {
+		return
+	}
+	file, err := os.Create(testTechFilePath)
+	testsuite.TestMainCheckError(err)
+
+	const url = "https://github.com/AliasIO/wappalyzer/blob/master/src/technologies.json"
+	client := http.Client{}
+	defer client.CloseIdleConnections()
+	resp, err := client.Get(url)
+	testsuite.TestMainCheckError(err)
+	defer func() { _ = resp.Body.Close() }()
+	_, err = io.Copy(file, resp.Body)
+	testsuite.TestMainCheckError(err)
+}
--- scanner/webinfo/webinfo_test.go
+++ scanner/webinfo/webinfo_test.go
@@ -1,1 +1,11 @@
 package webinfo
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	testDownloadWappalyzerData()
+	os.Exit(m.Run())
+}
