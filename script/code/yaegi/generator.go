4224586f5fe2000cf1eaa204724c6e6fc1cd6f56
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-12-11 20:43:39 +0800

    code/yaegi: add yaegi generator for process generated code, add TestExportGoRoot().

---
+++ script/code/yaegi/generator.go
@@ -0,0 +1,26 @@
+package yaegi
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/traefik/yaegi/extract"
+)
+
+func generateCode(pkg, init string) (string, error) {
+	ext := extract.Extractor{
+		Dest: "yaegi", // will be removed
+	}
+	buf := bytes.NewBuffer(make([]byte, 0, 10240))
+	_, err := ext.Extract(pkg, "", buf)
+	if err != nil {
+		return "", err
+	}
+	code := buf.String()
+	// remove all code before func init()
+	idx := strings.Index(code, "func init()")
+	code = code[idx:]
+	// rename init like initString() for search easily
+	code = strings.Replace(code, "init", "init"+init, 1)
+	return code, nil
+}
