de29d593b652138ad8987ec0d09d83771577a97a
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-12-22 19:55:10 +0800

    add sum script for rebuild go.sum file when upgrade module or add new module.

---
+++ sum.sh
@@ -0,0 +1,35 @@
+go get -d github.com/akamensky/argparse@v1.2.2
+go get -d github.com/bouk/monkey@v1.0.1
+go get -d github.com/creasty/defaults@v1.5.1
+go get -d github.com/davecgh/go-spew@v1.1.1
+go get -d github.com/go-ole/go-ole@v1.2.5
+go get -d github.com/go-sql-driver/mysql@v1.6.0
+go get -d github.com/gorilla/csrf@v1.7.0
+go get -d github.com/gorilla/mux@v1.8.0
+go get -d github.com/gorilla/sessions@v1.2.1
+go get -d github.com/gorilla/websocket@v1.4.2
+go get -d github.com/jinzhu/gorm@v1.9.16
+go get -d github.com/julienschmidt/httprouter@v1.3.0
+go get -d github.com/kardianos/service@v1.2.0
+go get -d github.com/looplab/fsm@v0.2.0
+go get -d github.com/lucas-clemente/quic-go@v0.20.0
+go get -d github.com/Microsoft/go-winio@v0.4.16
+go get -d github.com/pelletier/go-toml@v1.8.1
+go get -d github.com/pkg/errors@v0.9.1
+go get -d github.com/pmezard/go-difflib@v1.0.0
+go get -d github.com/stretchr/testify@v1.7.0
+go get -d github.com/traefik/yaegi@v0.9.15
+go get -d github.com/vmihailenco/msgpack/v5@v5.3.1
+go get -d github.com/xtaci/kcp-go/v5@v5.6.1
+
+go get -d github.com/axgle/mahonia@v0.0.0-20180208002826-3358181d7394
+go get -d github.com/gocolly/colly/v2@v2.1.1-0.20210315141944-34f9af5d9113
+go get -d github.com/mohae/deepcopy@v0.0.0-20170929034955-c48cc78d4826
+
+go get -d golang.org/x/arch@v0.0.0-20210127225635-455c95562d18
+go get -d golang.org/x/crypto@v0.0.0-20201221181555-eec23a3978ad
+go get -d golang.org/x/net@v0.0.0-20210119194325-5f4716e94777
+go get -d golang.org/x/sys@v0.0.0-20210124154548-22da62e12c0c
+go get -d golang.org/x/term@v0.0.0-20201210144234-2321bbc49cbf
+
+go get -d gopkg.in/check.v1@v1.0.0-20201130134442-10cb98267c6c
