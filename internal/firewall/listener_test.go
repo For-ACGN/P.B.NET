f50257ee444b3f2535844c03c37401eb9f478a82
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-12-31 20:08:56 +0800

    firewall/listener_test.go: improve TestListener() about filter mode, add testDial().

--- internal/firewall/listener_test.go
+++ internal/firewall/listener_test.go
@@ -9,6 +9,16 @@ import (
 	"project/internal/testsuite"
 )
 
+func testDial(t *testing.T, local, remote string) net.Conn {
+	lAddr, err := net.ResolveTCPAddr("tcp", local)
+	require.NoError(t, err)
+	rAddr, err := net.ResolveTCPAddr("tcp", remote)
+	require.NoError(t, err)
+	conn, err := net.DialTCP("tcp", lAddr, rAddr)
+	require.NoError(t, err)
+	return conn
+}
+
 func TestListener(t *testing.T) {
 	gm := testsuite.MarkGoroutines(t)
 	defer gm.Compare()
@@ -16,32 +26,139 @@ func TestListener(t *testing.T) {
 	t.Run("default mode-per host", func(t *testing.T) {
 		rawListener, err := net.Listen("tcp", "127.0.0.1:0")
 		require.NoError(t, err)
-		addr := rawListener.Addr().String()
 		opts := ListenerOptions{
 			FilterMode:      FilterModeDefault,
 			MaxConnsPerHost: 1,
 		}
 		listener, err := NewListener(rawListener, &opts)
 		require.NoError(t, err)
+		addr := listener.Addr().String()
+
+		go func() {
+			conn1, err := listener.Accept()
+			require.NoError(t, err)
+			conn2, err := listener.Accept()
+			require.NoError(t, err)
+
+			conn3, err := listener.Accept()
+			require.Error(t, err)
+			t.Log(err)
+			require.Nil(t, conn3)
+
+			err = conn1.Close()
+			require.NoError(t, err)
+			err = conn2.Close()
+			require.NoError(t, err)
+
+			testsuite.IsDestroyed(t, conn1)
+			testsuite.IsDestroyed(t, conn2)
+		}()
+
+		conn1 := testDial(t, "127.0.0.1:0", addr)
+		conn2 := testDial(t, "127.0.0.2:0", addr)
+		conn3 := testDial(t, "127.0.0.1:0", addr)
+
+		_, err = conn1.Read(make([]byte, 1024))
+		require.Error(t, err)
+		_, err = conn2.Read(make([]byte, 1024))
+		require.Error(t, err)
+
+		err = conn1.Close()
+		require.NoError(t, err)
+		err = conn2.Close()
+		require.NoError(t, err)
+		err = conn3.Close()
+		require.NoError(t, err)
+
+		err = listener.Close()
+		require.NoError(t, err)
+
+		testsuite.IsDestroyed(t, listener)
+	})
+
+	t.Run("default mode-total", func(t *testing.T) {
+		rawListener, err := net.Listen("tcp", "127.0.0.1:0")
+		require.NoError(t, err)
+		opts := ListenerOptions{
+			FilterMode:    FilterModeDefault,
+			MaxConnsTotal: 2,
+		}
+		listener, err := NewListener(rawListener, &opts)
+		require.NoError(t, err)
+		addr := listener.Addr().String()
+
+		go func() {
+			conn1, err := listener.Accept()
+			require.NoError(t, err)
+			conn2, err := listener.Accept()
+			require.NoError(t, err)
+
+			conn3, err := listener.Accept()
+			require.Error(t, err)
+			t.Log(err)
+			require.Nil(t, conn3)
+
+			err = conn1.Close()
+			require.NoError(t, err)
+			err = conn2.Close()
+			require.NoError(t, err)
+
+			testsuite.IsDestroyed(t, conn1)
+			testsuite.IsDestroyed(t, conn2)
+		}()
+
+		conn1 := testDial(t, "127.0.0.1:0", addr)
+		conn2 := testDial(t, "127.0.0.2:0", addr)
+		conn3 := testDial(t, "127.0.0.3:0", addr)
+
+		_, err = conn1.Read(make([]byte, 1024))
+		require.Error(t, err)
+		_, err = conn2.Read(make([]byte, 1024))
+		require.Error(t, err)
+		// conn3 is alive but listener can't accept it
+		// _, err = conn3.Read(make([]byte, 1024))
+		// require.Error(t, err)
+
+		err = conn1.Close()
+		require.NoError(t, err)
+		err = conn2.Close()
+		require.NoError(t, err)
+		err = conn3.Close()
+		require.NoError(t, err)
+
+		err = listener.Close()
+		require.NoError(t, err)
+
+		testsuite.IsDestroyed(t, listener)
+	})
+
+	t.Run("allow mode", func(t *testing.T) {
+		rawListener, err := net.Listen("tcp", "127.0.0.1:0")
+		require.NoError(t, err)
+		opts := ListenerOptions{
+			FilterMode: FilterModeAllow,
+		}
+		listener, err := NewListener(rawListener, &opts)
+		require.NoError(t, err)
+		addr := listener.Addr().String()
+		listener.AddAllowedHost("127.0.0.1")
 
 		go func() {
 			conn1, err := listener.Accept()
 			require.NoError(t, err)
 
-			conn2, err := listener.Accept()
-			require.Error(t, err)
-			require.Nil(t, conn2)
-
 			err = conn1.Close()
 			require.NoError(t, err)
 
 			testsuite.IsDestroyed(t, conn1)
+
+			conn2, err := listener.Accept()
+			require.Error(t, err)
+			require.Nil(t, conn2)
 		}()
 
-		conn1, err := net.Dial("tcp", addr)
-		require.NoError(t, err)
-		conn2, err := net.Dial("tcp", addr)
-		require.NoError(t, err)
+		conn1 := testDial(t, "127.0.0.1:0", addr)
+		conn2 := testDial(t, "127.0.0.2:0", addr)
 
 		_, err = conn1.Read(make([]byte, 1024))
 		require.Error(t, err)
@@ -59,42 +176,38 @@ func TestListener(t *testing.T) {
 		testsuite.IsDestroyed(t, listener)
 	})
 
-	t.Run("default mode-total", func(t *testing.T) {
+	t.Run("block mode", func(t *testing.T) {
 		rawListener, err := net.Listen("tcp", "127.0.0.1:0")
 		require.NoError(t, err)
-		addr := rawListener.Addr().String()
 		opts := ListenerOptions{
-			FilterMode:    FilterModeDefault,
-			MaxConnsTotal: 1,
+			FilterMode: FilterModeBlock,
 		}
 		listener, err := NewListener(rawListener, &opts)
 		require.NoError(t, err)
+		addr := listener.Addr().String()
+		listener.AddBlockedHost("127.0.0.2")
 
 		go func() {
 			conn1, err := listener.Accept()
 			require.NoError(t, err)
 
-			// inner conn2 is not accepted
-			conn2, err := listener.Accept()
-			require.Error(t, err)
-			require.Nil(t, conn2)
-
 			err = conn1.Close()
 			require.NoError(t, err)
 
 			testsuite.IsDestroyed(t, conn1)
+
+			conn2, err := listener.Accept()
+			require.Error(t, err)
+			require.Nil(t, conn2)
 		}()
 
-		conn1, err := net.Dial("tcp", addr)
-		require.NoError(t, err)
-		conn2, err := net.Dial("tcp", addr)
-		require.NoError(t, err)
+		conn1 := testDial(t, "127.0.0.1:0", addr)
+		conn2 := testDial(t, "127.0.0.2:0", addr)
 
 		_, err = conn1.Read(make([]byte, 1024))
 		require.Error(t, err)
-		// conn2 is alive
-		// _, err = conn2.Read(make([]byte, 1024))
-		// require.Error(t, err)
+		_, err = conn2.Read(make([]byte, 1024))
+		require.Error(t, err)
 
 		err = conn1.Close()
 		require.NoError(t, err)
@@ -105,13 +218,5 @@ func TestListener(t *testing.T) {
 		require.NoError(t, err)
 
 		testsuite.IsDestroyed(t, listener)
-	})
-
-	t.Run("allow mode", func(t *testing.T) {
-
-	})
-
-	t.Run("block mode", func(t *testing.T) {
-
 	})
 }
