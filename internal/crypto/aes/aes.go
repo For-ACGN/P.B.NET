3c6d8c26d63bf70dfa7a7390eacb90f265fa6c50
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-01-09 13:53:12 +0800

    crypto/aes/aes.go: add Encrypter interface, update comments about IVSize and BlockSize.

--- internal/crypto/aes/aes.go
+++ internal/crypto/aes/aes.go
@@ -4,16 +4,18 @@ import (
 	"errors"
 )
 
-// about valid AES key size.
+// about AES valid key size.
 const (
 	Key128Bit int = 16
 	Key192Bit int = 24
 	Key256Bit int = 32
 )
 
-// about AES information.
 const (
-	IVSize    int = 16
+	// IVSize is the AES IV size.
+	IVSize int = 16
+
+	// BlockSize is the AES block size in bytes.
 	BlockSize int = 16
 )
 
@@ -23,3 +25,15 @@ var (
 	ErrInvalidCipherData  = errors.New("invalid aes cipher data")
 	ErrInvalidPaddingSize = errors.New("invalid aes padding size")
 )
+
+// Encrypter is a aes encrypter, it can encrypt and decrypt data.
+type Encrypter interface {
+	// Encrypt is used to encrypt data.
+	Encrypt(data []byte) ([]byte, error)
+
+	// Decrypt is used to decrypt data.
+	Decrypt(data []byte) ([]byte, error)
+
+	// Key is used to get aes key.
+	Key() []byte
+}
