3b66b96188d307d154ea4c8071ae5544b8ff446e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-01-11 20:32:43 +0800

    crypto/lsb/lsb_test.go: add tests, TestMode_String() and TestWriterAndReader().

--- internal/crypto/lsb/lsb_test.go
+++ internal/crypto/lsb/lsb_test.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"io/ioutil"
 	"math"
+	"os"
 	"reflect"
 	"testing"
 	"unsafe"
@@ -20,6 +21,116 @@ import (
 	"project/internal/random"
 	"project/internal/testsuite"
 )
+
+var tests = [...]*struct {
+	mode      Mode
+	newWriter func(img image.Image) (Writer, error)
+	newReader func(img []byte) (Reader, error)
+}{
+	{
+		PNGWithNRGBA32,
+		func(img image.Image) (Writer, error) {
+			return NewPNGWriter(img, PNGWithNRGBA32)
+		},
+		func(img []byte) (Reader, error) {
+			return NewPNGReader(img)
+		},
+	},
+	{
+		PNGWithNRGBA64,
+		func(img image.Image) (Writer, error) {
+			return NewPNGWriter(img, PNGWithNRGBA64)
+		},
+		func(img []byte) (Reader, error) {
+			return NewPNGReader(img)
+		},
+	},
+}
+
+func TestMode_String(t *testing.T) {
+	for _, test := range tests {
+		fmt.Println(test.mode)
+	}
+	fmt.Println(Mode(1234578))
+}
+
+func TestWriterAndReader(t *testing.T) {
+	t.Run("black", func(t *testing.T) { testWriterAndReader(t, "black") })
+	t.Run("white", func(t *testing.T) { testWriterAndReader(t, "white") })
+}
+
+func testWriterAndReader(t *testing.T, name string) {
+	file, err := os.Open(fmt.Sprintf("testdata/%s.png", name))
+	require.NoError(t, err)
+	defer func() { _ = file.Close() }()
+	img, err := png.Decode(file)
+	require.NoError(t, err)
+
+	for _, test := range tests {
+		t.Run(test.mode.String(), func(t *testing.T) {
+			t.Run("common", func(t *testing.T) {
+				testdata1 := random.Bytes(256 + random.Int(256))
+				testdata2 := random.Bytes(512 + random.Int(512))
+				testdata1Len := len(testdata1)
+				testdata2Len := len(testdata2)
+
+				// write data
+				writer, err := test.newWriter(img)
+				require.NoError(t, err)
+
+				n, err := writer.Write(testdata1)
+				require.NoError(t, err)
+				require.Equal(t, testdata1Len, n)
+				n, err = writer.Write(testdata2)
+				require.NoError(t, err)
+				require.Equal(t, testdata2Len, n)
+
+				output := bytes.NewBuffer(make([]byte, 0, 8192))
+				err = writer.Encode(output)
+				require.NoError(t, err)
+
+				require.Equal(t, img, writer.Image())
+
+				// read data
+				reader, err := test.newReader(output.Bytes())
+				require.NoError(t, err)
+
+				rv := 64 + random.Int(64)
+				buf1 := make([]byte, testdata1Len-rv)
+				buf2 := make([]byte, testdata2Len+rv)
+				_, err = io.ReadFull(reader, buf1)
+				require.NoError(t, err)
+				_, err = io.ReadFull(reader, buf2)
+				require.NoError(t, err)
+				result := append(buf1, buf2...)
+
+				expected := append(testdata1, testdata2...)
+				require.Equal(t, expected, result)
+
+				// compare image with output
+				outputPNG, err := png.Decode(bytes.NewReader(output.Bytes()))
+				require.NoError(t, err)
+				require.Equal(t, outputPNG, reader.Image())
+
+				require.Equal(t, writer.Mode(), reader.Mode())
+
+				testsuite.IsDestroyed(t, writer)
+			})
+
+			t.Run("reset", func(t *testing.T) {
+
+			})
+
+			t.Run("offset", func(t *testing.T) {
+
+			})
+		})
+	}
+}
+
+func TestEncrypterAndDecrypter(t *testing.T) {
+
+}
 
 func TestCalculateStorageSize(t *testing.T) {
 	for _, testdata := range [...]*struct {
