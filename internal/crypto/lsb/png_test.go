b4ebd8ec3fac17485399e57734aa4a285ac3085c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-01-18 20:57:32 +0800

    crypto/lsb/png_test.go: add TestNewPNGReader, TestPNGReaderWithInvalidMode and TestNewPNGEncrypter.

--- internal/crypto/lsb/png_test.go
+++ internal/crypto/lsb/png_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"image"
 	"image/png"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -59,4 +60,76 @@ func TestPNGWriterWithInvalidMode(t *testing.T) {
 	})
 
 	testsuite.IsDestroyed(t, writer)
+}
+
+func TestNewPNGReader(t *testing.T) {
+	t.Run("invalid image", func(t *testing.T) {
+		reader, err := NewPNGReader(nil)
+		require.Error(t, err)
+		require.Nil(t, reader)
+	})
+
+	t.Run("unsupported png format", func(t *testing.T) {
+		data, err := os.ReadFile("testdata/black.png")
+		require.NoError(t, err)
+
+		reader, err := NewPNGReader(data)
+		require.EqualError(t, err, "unsupported png format: *image.RGBA")
+		require.Nil(t, reader)
+	})
+}
+
+func TestPNGReaderWithInvalidMode(t *testing.T) {
+	img := testGeneratePNGBytes(t, 160, 90)
+	reader, err := NewPNGReader(img)
+	require.NoError(t, err)
+	reader.mode = InvalidMode
+
+	t.Run("Read", func(t *testing.T) {
+		defer testsuite.DeferForPanic(t)
+
+		_, err = reader.Read(make([]byte, 1024))
+		require.Error(t, err)
+	})
+
+	testsuite.IsDestroyed(t, reader)
+}
+
+func TestNewPNGEncrypter(t *testing.T) {
+	t.Run("unknown mode", func(t *testing.T) {
+		img := testGeneratePNG(160, 90)
+
+		encrypter, err := NewPNGEncrypter(img, 0, nil)
+		require.EqualError(t, err, "png writer with unknown mode: 0")
+		require.Nil(t, encrypter)
+	})
+
+	t.Run("too small image", func(t *testing.T) {
+		img := testGeneratePNG(1, 2)
+
+		encrypter, err := NewPNGEncrypter(img, PNGWithNRGBA32, nil)
+		require.Equal(t, err, ErrImgTooSmall)
+		require.Nil(t, encrypter)
+	})
+
+	t.Run("failed to reset", func(t *testing.T) {
+		img := testGeneratePNG(160, 90)
+		invalidKey := make([]byte, 8)
+
+		encrypter, err := NewPNGEncrypter(img, PNGWithNRGBA32, invalidKey)
+		require.Error(t, err)
+		require.Nil(t, encrypter)
+	})
+}
+
+func TestPNGEncrypter_Write(t *testing.T) {
+
+}
+
+func TestPNGEncrypter_Encode(t *testing.T) {
+
+}
+
+func TestPNGEncrypter_SetOffset(t *testing.T) {
+
 }
