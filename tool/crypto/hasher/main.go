cb5670485203b7b93e5f02e95fa9fd234cd99979
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-01-23 14:37:44 +0800

    crypto/hasher/main.go: improve init function and update printAlgorithmAndDigest.

--- tool/crypto/hasher/main.go
+++ tool/crypto/hasher/main.go
@@ -15,8 +15,18 @@ import (
 	"os"
 	"strings"
 
+	"project/internal/convert"
 	"project/internal/system"
 )
+
+var algorithmIdx = []string{
+	"md5",
+	"sha1",
+	"sha224",
+	"sha256",
+	"sha384",
+	"sha512",
+}
 
 var algorithms = map[string]func() hash.Hash{
 	"md5":    md5.New,
@@ -31,36 +41,40 @@ var (
 	alg  string
 	text string
 	file string
-	key  []byte
+	key  string
 )
 
 func init() {
 	flag.CommandLine.SetOutput(os.Stdout)
 	flag.CommandLine.Usage = printUsage
 
-	var keyStr string
-
-	flag.StringVar(&alg, "alg", "", "select hash algorithm")
-	flag.StringVar(&text, "text", "hello", "input text message")
-	flag.StringVar(&file, "file", "", "input file")
-	flag.StringVar(&keyStr, "key", "", "key for hmac algorithm")
+	flag.StringVar(&alg, "alg", "", "specify hash algorithm")
+	flag.StringVar(&text, "text", "", "input text message")
+	flag.StringVar(&file, "file", "", "input file path")
+	flag.StringVar(&key, "key", "", "key for hmac algorithm")
 	flag.Parse()
 
 	// add hmac algorithm
-	key = []byte(keyStr)
-	for a, fn := range algorithms {
-		f := fn
-		algorithms["hmac-"+a] = func() hash.Hash {
-			return hmac.New(f, key)
+	algList := make([]string, len(algorithmIdx))
+	keyBytes := []byte(key)
+	for i, alg := range algorithmIdx {
+		fn, ok := algorithms[alg]
+		if !ok {
+			panic(fmt.Sprintf("algorithm %s is not exist", alg))
 		}
+		algorithms["hmac-"+alg] = func() hash.Hash {
+			return hmac.New(fn, keyBytes)
+		}
+		algList[i] = "hmac-" + alg
 	}
+	algorithmIdx = append(algorithmIdx, algList...)
 }
 
 func printUsage() {
 	exe, err := system.ExecutableName()
 	system.CheckError(err)
 	const format = `
-supported algorithm:
+supported algorithms:
   md5, sha1, sha224, sha256, sha384, sha512,
   hmac-md5, hmac-sha1, hmac-sha224, hmac-sha256
   hmac-sha384, hmac-sha512
@@ -68,18 +82,22 @@ supported algorithm:
 usage:
   %s -alg "sha256" -text "hello"
   %s -alg "sha256" -file "f.txt"
-
   %s -alg "hmac-sha256" -text "hello" -key "hash"
   %s -alg "hmac-sha256" -file "f.txt" -key "hash"
 
-  If -alg is empty, use all supported algorithm.
+  If -alg is empty, use all supported algorithms.
 `
 	fmt.Printf(format[1:]+"\n", exe, exe, exe, exe)
 	flag.PrintDefaults()
 }
 
 func main() {
-	// select algorithm
+	// check arguments
+	if len(os.Args) == 1 {
+		printUsage()
+		return
+	}
+	// specify algorithm
 	fn, ok := algorithms[alg]
 	if !ok && alg != "" {
 		system.PrintErrorf("unsupported algorithm: %s", alg)
@@ -89,49 +107,62 @@ func main() {
 		fmt.Printf("[warning] use %s algorithm, but key is empty\n\n", alg)
 	}
 	// create data reader
-	var rs io.ReadSeeker
+	var (
+		rs     io.ReadSeeker
+		buffer []byte
+	)
 	if file != "" {
 		f, err := os.Open(file)
 		system.CheckError(err)
 		defer func() { _ = f.Close() }()
 		rs = f
+		buffer = make([]byte, 1024*1024)
 	} else {
 		rs = strings.NewReader(text)
+		buffer = make([]byte, 1024)
 	}
-	// use single algorithm
+	// use specific algorithm
 	if fn != nil {
 		h := fn()
-		_, err := io.Copy(h, rs)
+		_, err := io.CopyBuffer(h, rs, buffer)
 		system.CheckError(err)
 		digest := h.Sum(nil)
 		printAlgorithmAndDigest(alg, digest)
 		return
 	}
 	// use all algorithms
-	for alg, fn := range algorithms {
-		h := fn()
-		_, err := io.Copy(h, rs)
+	for _, alg := range algorithmIdx {
+		h := algorithms[alg]()
+		_, err := io.CopyBuffer(h, rs, buffer)
 		system.CheckError(err)
 		digest := h.Sum(nil)
 		printAlgorithmAndDigest(alg, digest)
+		_, err = rs.Seek(0, io.SeekStart)
+		system.CheckError(err)
 	}
 }
 
 func printAlgorithmAndDigest(alg string, digest []byte) {
 	hexStr := hex.EncodeToString(digest)
 	hexStrUp := strings.ToUpper(hexStr)
+	base64Str := base64.StdEncoding.EncodeToString(digest)
+
+	prefix := strings.Repeat(" ", 11)
+	hexLow := convert.SdumpStringWithPL(hexStr, prefix, 64)
+	hexUp := convert.SdumpStringWithPL(hexStrUp, prefix, 64)
+	base64St := convert.SdumpStringWithPL(base64Str, prefix, 64)
+	bytesStr := convert.SdumpBytesWithPL(digest, prefix, 8)
 
 	fmt.Println("algorithm:", alg)
-	fmt.Printf("  raw:     %v\n", digest)
-	fmt.Printf("  hex-low: %s\n", hexStr)
-	fmt.Printf("  hex-up:  %s\n", hexStrUp)
-	fmt.Printf("  base64:  %s\n", base64.StdEncoding.EncodeToString(digest))
-
+	fmt.Printf("  hex-low: %s\n", strings.Replace(hexLow, prefix, "", 1))
+	fmt.Printf("  hex-up:  %s\n", strings.Replace(hexUp, prefix, "", 1))
+	fmt.Printf("  base64:  %s\n", strings.Replace(base64St, prefix, "", 1))
+	fmt.Printf("  bytes:   %s\n", strings.Replace(bytesStr, prefix, "", 1))
 	if strings.Contains(alg, "md5") {
-		fmt.Println()
-		fmt.Printf("  hex-low 16: %s\n", hexStr[8:24])
-		fmt.Printf("  hex-up  16: %s\n", hexStrUp[8:24])
+		fmt.Println("  ------------------------------------------------")
+		fmt.Println("  hex-low 16:", hexStr[8:24])
+		fmt.Println("  hex-up  16:", hexStrUp[8:24])
+		fmt.Println("  ------------------------------------------------")
 	}
-
 	fmt.Println()
 }
