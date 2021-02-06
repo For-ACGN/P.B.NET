af7a72e6a3d831735f2d9d00dd610991e45e1ee9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-02-06 08:19:37 +0800

    certificate/manager: refactor certmgr for test easily, add testdata, remove old files.

--- tool/certificate/manager/cmd/main.go
+++ tool/certificate/manager/cmd/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"flag"
+	"os"
+)
+
+var (
+	initMgr  bool
+	resetPwd bool
+	filePath string
+)
+
+func init() {
+	flag.CommandLine.SetOutput(os.Stdout)
+
+	flag.BoolVar(&initMgr, "init", false, "initialize certificate manager")
+	flag.BoolVar(&resetPwd, "reset", false, "reset certificate manager password")
+	flag.StringVar(&filePath, "file", "key/certpool.bin", "certificate pool file")
+	flag.Parse()
+}
+
+func main() {
+	switch {
+	case initMgr:
+
+	case resetPwd:
+
+	default:
+
+	}
+}
