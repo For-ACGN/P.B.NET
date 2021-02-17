7674caa7128c4e1103797aa3a21aa599069a4594
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-02-17 13:00:32 +0800

    system/terminal.go: move functions and tests about terminal to terminal.go.

--- internal/system/program_test.go
+++ internal/system/program_test.go
@@ -105,29 +105,3 @@ func TestPrintErrorf(t *testing.T) {
 
 	PrintErrorf("error: %s\n", "test error")
 }
-
-func TestCommandLineToArgv(t *testing.T) {
-	exe1 := "test"
-	exe2 := `"test test"`
-	for _, testdata := range [...]*struct {
-		cmd  string
-		args []string
-	}{
-		{"net", []string{"net"}},
-		{`net -a -b`, []string{"net", "-a", "-b"}},
-		{`net -a -b "a"`, []string{"net", "-a", "-b", "a"}},
-		{`"net net"`, []string{"net net"}},
-		{`"net\net"`, []string{`net\net`}},
-		{`"net\net net"`, []string{`net\net net`}},
-		{`net -a \"net`, []string{"net", "-a", `"net`}},
-		{`net -a ""`, []string{"net", "-a", ""}},
-		{`""net""  -a  -b`, []string{"net", "-a", "-b"}},
-		{`"""net""" -a`, []string{`"net"`, "-a"}},
-	} {
-		args := CommandLineToArgv(exe1 + " " + testdata.cmd)
-		require.Equal(t, append([]string{"test"}, testdata.args...), args)
-
-		args = CommandLineToArgv(exe2 + " " + testdata.cmd)
-		require.Equal(t, append([]string{"test test"}, testdata.args...), args)
-	}
-}
