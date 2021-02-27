86be57c8276b542fed8b0ee77842c7a5f74fab93
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-02-27 12:56:35 +0800

    interpreter/yaegi/goroot/bundle_*.go: add packages about debug/gosym, macho, pe and plan9obj.

--- internal/interpreter/yaegi/goroot/bundle_go1_12.go
+++ internal/interpreter/yaegi/goroot/bundle_go1_12.go
@@ -40,6 +40,10 @@ import (
 	"database/sql"
 	"debug/dwarf"
 	"debug/elf"
+	"debug/gosym"
+	"debug/macho"
+	"debug/pe"
+	"debug/plan9obj"
 	"encoding"
 	"encoding/ascii85"
 	"encoding/asn1"
@@ -100,6 +104,10 @@ func init() {
 	init_database_sql()
 	init_debug_dwarf()
 	init_debug_elf()
+	init_debug_gosym()
+	init_debug_macho()
+	init_debug_pe()
+	init_debug_plan9obj()
 	init_encoding()
 	init_encoding_ascii85()
 	init_encoding_asn1()
@@ -2602,6 +2610,251 @@ func init_debug_elf() {
 		"Symbol":          reflect.ValueOf((*elf.Symbol)(nil)),
 		"Type":            reflect.ValueOf((*elf.Type)(nil)),
 		"Version":         reflect.ValueOf((*elf.Version)(nil)),
+	}
+}
+
+func init_debug_gosym() {
+	Symbols["debug/gosym"] = map[string]reflect.Value{
+		// function, constant and variable definitions
+		"NewLineTable": reflect.ValueOf(gosym.NewLineTable),
+		"NewTable":     reflect.ValueOf(gosym.NewTable),
+
+		// type definitions
+		"DecodingError":    reflect.ValueOf((*gosym.DecodingError)(nil)),
+		"Func":             reflect.ValueOf((*gosym.Func)(nil)),
+		"LineTable":        reflect.ValueOf((*gosym.LineTable)(nil)),
+		"Obj":              reflect.ValueOf((*gosym.Obj)(nil)),
+		"Sym":              reflect.ValueOf((*gosym.Sym)(nil)),
+		"Table":            reflect.ValueOf((*gosym.Table)(nil)),
+		"UnknownFileError": reflect.ValueOf((*gosym.UnknownFileError)(nil)),
+		"UnknownLineError": reflect.ValueOf((*gosym.UnknownLineError)(nil)),
+	}
+}
+
+func init_debug_macho() {
+	Symbols["debug/macho"] = map[string]reflect.Value{
+		// function, constant and variable definitions
+		"ARM64_RELOC_ADDEND":              reflect.ValueOf(macho.ARM64_RELOC_ADDEND),
+		"ARM64_RELOC_BRANCH26":            reflect.ValueOf(macho.ARM64_RELOC_BRANCH26),
+		"ARM64_RELOC_GOT_LOAD_PAGE21":     reflect.ValueOf(macho.ARM64_RELOC_GOT_LOAD_PAGE21),
+		"ARM64_RELOC_GOT_LOAD_PAGEOFF12":  reflect.ValueOf(macho.ARM64_RELOC_GOT_LOAD_PAGEOFF12),
+		"ARM64_RELOC_PAGE21":              reflect.ValueOf(macho.ARM64_RELOC_PAGE21),
+		"ARM64_RELOC_PAGEOFF12":           reflect.ValueOf(macho.ARM64_RELOC_PAGEOFF12),
+		"ARM64_RELOC_POINTER_TO_GOT":      reflect.ValueOf(macho.ARM64_RELOC_POINTER_TO_GOT),
+		"ARM64_RELOC_SUBTRACTOR":          reflect.ValueOf(macho.ARM64_RELOC_SUBTRACTOR),
+		"ARM64_RELOC_TLVP_LOAD_PAGE21":    reflect.ValueOf(macho.ARM64_RELOC_TLVP_LOAD_PAGE21),
+		"ARM64_RELOC_TLVP_LOAD_PAGEOFF12": reflect.ValueOf(macho.ARM64_RELOC_TLVP_LOAD_PAGEOFF12),
+		"ARM64_RELOC_UNSIGNED":            reflect.ValueOf(macho.ARM64_RELOC_UNSIGNED),
+		"ARM_RELOC_BR24":                  reflect.ValueOf(macho.ARM_RELOC_BR24),
+		"ARM_RELOC_HALF":                  reflect.ValueOf(macho.ARM_RELOC_HALF),
+		"ARM_RELOC_HALF_SECTDIFF":         reflect.ValueOf(macho.ARM_RELOC_HALF_SECTDIFF),
+		"ARM_RELOC_LOCAL_SECTDIFF":        reflect.ValueOf(macho.ARM_RELOC_LOCAL_SECTDIFF),
+		"ARM_RELOC_PAIR":                  reflect.ValueOf(macho.ARM_RELOC_PAIR),
+		"ARM_RELOC_PB_LA_PTR":             reflect.ValueOf(macho.ARM_RELOC_PB_LA_PTR),
+		"ARM_RELOC_SECTDIFF":              reflect.ValueOf(macho.ARM_RELOC_SECTDIFF),
+		"ARM_RELOC_VANILLA":               reflect.ValueOf(macho.ARM_RELOC_VANILLA),
+		"ARM_THUMB_32BIT_BRANCH":          reflect.ValueOf(macho.ARM_THUMB_32BIT_BRANCH),
+		"ARM_THUMB_RELOC_BR22":            reflect.ValueOf(macho.ARM_THUMB_RELOC_BR22),
+		"Cpu386":                          reflect.ValueOf(macho.Cpu386),
+		"CpuAmd64":                        reflect.ValueOf(macho.CpuAmd64),
+		"CpuArm":                          reflect.ValueOf(macho.CpuArm),
+		"CpuArm64":                        reflect.ValueOf(macho.CpuArm64),
+		"CpuPpc":                          reflect.ValueOf(macho.CpuPpc),
+		"CpuPpc64":                        reflect.ValueOf(macho.CpuPpc64),
+		"ErrNotFat":                       reflect.ValueOf(&macho.ErrNotFat).Elem(),
+		"FlagAllModsBound":                reflect.ValueOf(macho.FlagAllModsBound),
+		"FlagAllowStackExecution":         reflect.ValueOf(macho.FlagAllowStackExecution),
+		"FlagAppExtensionSafe":            reflect.ValueOf(macho.FlagAppExtensionSafe),
+		"FlagBindAtLoad":                  reflect.ValueOf(macho.FlagBindAtLoad),
+		"FlagBindsToWeak":                 reflect.ValueOf(macho.FlagBindsToWeak),
+		"FlagCanonical":                   reflect.ValueOf(macho.FlagCanonical),
+		"FlagDeadStrippableDylib":         reflect.ValueOf(macho.FlagDeadStrippableDylib),
+		"FlagDyldLink":                    reflect.ValueOf(macho.FlagDyldLink),
+		"FlagForceFlat":                   reflect.ValueOf(macho.FlagForceFlat),
+		"FlagHasTLVDescriptors":           reflect.ValueOf(macho.FlagHasTLVDescriptors),
+		"FlagIncrLink":                    reflect.ValueOf(macho.FlagIncrLink),
+		"FlagLazyInit":                    reflect.ValueOf(macho.FlagLazyInit),
+		"FlagNoFixPrebinding":             reflect.ValueOf(macho.FlagNoFixPrebinding),
+		"FlagNoHeapExecution":             reflect.ValueOf(macho.FlagNoHeapExecution),
+		"FlagNoMultiDefs":                 reflect.ValueOf(macho.FlagNoMultiDefs),
+		"FlagNoReexportedDylibs":          reflect.ValueOf(macho.FlagNoReexportedDylibs),
+		"FlagNoUndefs":                    reflect.ValueOf(macho.FlagNoUndefs),
+		"FlagPIE":                         reflect.ValueOf(macho.FlagPIE),
+		"FlagPrebindable":                 reflect.ValueOf(macho.FlagPrebindable),
+		"FlagPrebound":                    reflect.ValueOf(macho.FlagPrebound),
+		"FlagRootSafe":                    reflect.ValueOf(macho.FlagRootSafe),
+		"FlagSetuidSafe":                  reflect.ValueOf(macho.FlagSetuidSafe),
+		"FlagSplitSegs":                   reflect.ValueOf(macho.FlagSplitSegs),
+		"FlagSubsectionsViaSymbols":       reflect.ValueOf(macho.FlagSubsectionsViaSymbols),
+		"FlagTwoLevel":                    reflect.ValueOf(macho.FlagTwoLevel),
+		"FlagWeakDefines":                 reflect.ValueOf(macho.FlagWeakDefines),
+		"GENERIC_RELOC_LOCAL_SECTDIFF":    reflect.ValueOf(macho.GENERIC_RELOC_LOCAL_SECTDIFF),
+		"GENERIC_RELOC_PAIR":              reflect.ValueOf(macho.GENERIC_RELOC_PAIR),
+		"GENERIC_RELOC_PB_LA_PTR":         reflect.ValueOf(macho.GENERIC_RELOC_PB_LA_PTR),
+		"GENERIC_RELOC_SECTDIFF":          reflect.ValueOf(macho.GENERIC_RELOC_SECTDIFF),
+		"GENERIC_RELOC_TLV":               reflect.ValueOf(macho.GENERIC_RELOC_TLV),
+		"GENERIC_RELOC_VANILLA":           reflect.ValueOf(macho.GENERIC_RELOC_VANILLA),
+		"LoadCmdDylib":                    reflect.ValueOf(macho.LoadCmdDylib),
+		"LoadCmdDylinker":                 reflect.ValueOf(macho.LoadCmdDylinker),
+		"LoadCmdDysymtab":                 reflect.ValueOf(macho.LoadCmdDysymtab),
+		"LoadCmdRpath":                    reflect.ValueOf(macho.LoadCmdRpath),
+		"LoadCmdSegment":                  reflect.ValueOf(macho.LoadCmdSegment),
+		"LoadCmdSegment64":                reflect.ValueOf(macho.LoadCmdSegment64),
+		"LoadCmdSymtab":                   reflect.ValueOf(macho.LoadCmdSymtab),
+		"LoadCmdThread":                   reflect.ValueOf(macho.LoadCmdThread),
+		"LoadCmdUnixThread":               reflect.ValueOf(macho.LoadCmdUnixThread),
+		"Magic32":                         reflect.ValueOf(macho.Magic32),
+		"Magic64":                         reflect.ValueOf(macho.Magic64),
+		"MagicFat":                        reflect.ValueOf(macho.MagicFat),
+		"NewFatFile":                      reflect.ValueOf(macho.NewFatFile),
+		"NewFile":                         reflect.ValueOf(macho.NewFile),
+		"Open":                            reflect.ValueOf(macho.Open),
+		"OpenFat":                         reflect.ValueOf(macho.OpenFat),
+		"TypeBundle":                      reflect.ValueOf(macho.TypeBundle),
+		"TypeDylib":                       reflect.ValueOf(macho.TypeDylib),
+		"TypeExec":                        reflect.ValueOf(macho.TypeExec),
+		"TypeObj":                         reflect.ValueOf(macho.TypeObj),
+		"X86_64_RELOC_BRANCH":             reflect.ValueOf(macho.X86_64_RELOC_BRANCH),
+		"X86_64_RELOC_GOT":                reflect.ValueOf(macho.X86_64_RELOC_GOT),
+		"X86_64_RELOC_GOT_LOAD":           reflect.ValueOf(macho.X86_64_RELOC_GOT_LOAD),
+		"X86_64_RELOC_SIGNED":             reflect.ValueOf(macho.X86_64_RELOC_SIGNED),
+		"X86_64_RELOC_SIGNED_1":           reflect.ValueOf(macho.X86_64_RELOC_SIGNED_1),
+		"X86_64_RELOC_SIGNED_2":           reflect.ValueOf(macho.X86_64_RELOC_SIGNED_2),
+		"X86_64_RELOC_SIGNED_4":           reflect.ValueOf(macho.X86_64_RELOC_SIGNED_4),
+		"X86_64_RELOC_SUBTRACTOR":         reflect.ValueOf(macho.X86_64_RELOC_SUBTRACTOR),
+		"X86_64_RELOC_TLV":                reflect.ValueOf(macho.X86_64_RELOC_TLV),
+		"X86_64_RELOC_UNSIGNED":           reflect.ValueOf(macho.X86_64_RELOC_UNSIGNED),
+
+		// type definitions
+		"Cpu":              reflect.ValueOf((*macho.Cpu)(nil)),
+		"Dylib":            reflect.ValueOf((*macho.Dylib)(nil)),
+		"DylibCmd":         reflect.ValueOf((*macho.DylibCmd)(nil)),
+		"Dysymtab":         reflect.ValueOf((*macho.Dysymtab)(nil)),
+		"DysymtabCmd":      reflect.ValueOf((*macho.DysymtabCmd)(nil)),
+		"FatArch":          reflect.ValueOf((*macho.FatArch)(nil)),
+		"FatArchHeader":    reflect.ValueOf((*macho.FatArchHeader)(nil)),
+		"FatFile":          reflect.ValueOf((*macho.FatFile)(nil)),
+		"File":             reflect.ValueOf((*macho.File)(nil)),
+		"FileHeader":       reflect.ValueOf((*macho.FileHeader)(nil)),
+		"FormatError":      reflect.ValueOf((*macho.FormatError)(nil)),
+		"Load":             reflect.ValueOf((*macho.Load)(nil)),
+		"LoadBytes":        reflect.ValueOf((*macho.LoadBytes)(nil)),
+		"LoadCmd":          reflect.ValueOf((*macho.LoadCmd)(nil)),
+		"Nlist32":          reflect.ValueOf((*macho.Nlist32)(nil)),
+		"Nlist64":          reflect.ValueOf((*macho.Nlist64)(nil)),
+		"Regs386":          reflect.ValueOf((*macho.Regs386)(nil)),
+		"RegsAMD64":        reflect.ValueOf((*macho.RegsAMD64)(nil)),
+		"Reloc":            reflect.ValueOf((*macho.Reloc)(nil)),
+		"RelocTypeARM":     reflect.ValueOf((*macho.RelocTypeARM)(nil)),
+		"RelocTypeARM64":   reflect.ValueOf((*macho.RelocTypeARM64)(nil)),
+		"RelocTypeGeneric": reflect.ValueOf((*macho.RelocTypeGeneric)(nil)),
+		"RelocTypeX86_64":  reflect.ValueOf((*macho.RelocTypeX86_64)(nil)),
+		"Rpath":            reflect.ValueOf((*macho.Rpath)(nil)),
+		"RpathCmd":         reflect.ValueOf((*macho.RpathCmd)(nil)),
+		"Section":          reflect.ValueOf((*macho.Section)(nil)),
+		"Section32":        reflect.ValueOf((*macho.Section32)(nil)),
+		"Section64":        reflect.ValueOf((*macho.Section64)(nil)),
+		"SectionHeader":    reflect.ValueOf((*macho.SectionHeader)(nil)),
+		"Segment":          reflect.ValueOf((*macho.Segment)(nil)),
+		"Segment32":        reflect.ValueOf((*macho.Segment32)(nil)),
+		"Segment64":        reflect.ValueOf((*macho.Segment64)(nil)),
+		"SegmentHeader":    reflect.ValueOf((*macho.SegmentHeader)(nil)),
+		"Symbol":           reflect.ValueOf((*macho.Symbol)(nil)),
+		"Symtab":           reflect.ValueOf((*macho.Symtab)(nil)),
+		"SymtabCmd":        reflect.ValueOf((*macho.SymtabCmd)(nil)),
+		"Thread":           reflect.ValueOf((*macho.Thread)(nil)),
+		"Type":             reflect.ValueOf((*macho.Type)(nil)),
+
+		// interface wrapper definitions
+		"_Load": reflect.ValueOf((*_debug_macho_Load)(nil)),
+	}
+}
+
+// _debug_macho_Load is an interface wrapper for Load type
+type _debug_macho_Load struct {
+	WRaw func() []byte
+}
+
+func (W _debug_macho_Load) Raw() []byte { return W.WRaw() }
+
+func init_debug_pe() {
+	Symbols["debug/pe"] = map[string]reflect.Value{
+		// function, constant and variable definitions
+		"COFFSymbolSize":                       reflect.ValueOf(constant.MakeFromLiteral("18", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_ARCHITECTURE":   reflect.ValueOf(constant.MakeFromLiteral("7", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_BASERELOC":      reflect.ValueOf(constant.MakeFromLiteral("5", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT":   reflect.ValueOf(constant.MakeFromLiteral("11", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR": reflect.ValueOf(constant.MakeFromLiteral("14", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_DEBUG":          reflect.ValueOf(constant.MakeFromLiteral("6", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT":   reflect.ValueOf(constant.MakeFromLiteral("13", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_EXCEPTION":      reflect.ValueOf(constant.MakeFromLiteral("3", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_EXPORT":         reflect.ValueOf(constant.MakeFromLiteral("0", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_GLOBALPTR":      reflect.ValueOf(constant.MakeFromLiteral("8", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_IAT":            reflect.ValueOf(constant.MakeFromLiteral("12", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_IMPORT":         reflect.ValueOf(constant.MakeFromLiteral("1", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG":    reflect.ValueOf(constant.MakeFromLiteral("10", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_RESOURCE":       reflect.ValueOf(constant.MakeFromLiteral("2", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_SECURITY":       reflect.ValueOf(constant.MakeFromLiteral("4", token.INT, 0)),
+		"IMAGE_DIRECTORY_ENTRY_TLS":            reflect.ValueOf(constant.MakeFromLiteral("9", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_AM33":              reflect.ValueOf(constant.MakeFromLiteral("467", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_AMD64":             reflect.ValueOf(constant.MakeFromLiteral("34404", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_ARM":               reflect.ValueOf(constant.MakeFromLiteral("448", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_ARM64":             reflect.ValueOf(constant.MakeFromLiteral("43620", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_ARMNT":             reflect.ValueOf(constant.MakeFromLiteral("452", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_EBC":               reflect.ValueOf(constant.MakeFromLiteral("3772", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_I386":              reflect.ValueOf(constant.MakeFromLiteral("332", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_IA64":              reflect.ValueOf(constant.MakeFromLiteral("512", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_M32R":              reflect.ValueOf(constant.MakeFromLiteral("36929", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_MIPS16":            reflect.ValueOf(constant.MakeFromLiteral("614", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_MIPSFPU":           reflect.ValueOf(constant.MakeFromLiteral("870", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_MIPSFPU16":         reflect.ValueOf(constant.MakeFromLiteral("1126", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_POWERPC":           reflect.ValueOf(constant.MakeFromLiteral("496", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_POWERPCFP":         reflect.ValueOf(constant.MakeFromLiteral("497", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_R4000":             reflect.ValueOf(constant.MakeFromLiteral("358", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_SH3":               reflect.ValueOf(constant.MakeFromLiteral("418", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_SH3DSP":            reflect.ValueOf(constant.MakeFromLiteral("419", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_SH4":               reflect.ValueOf(constant.MakeFromLiteral("422", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_SH5":               reflect.ValueOf(constant.MakeFromLiteral("424", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_THUMB":             reflect.ValueOf(constant.MakeFromLiteral("450", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_UNKNOWN":           reflect.ValueOf(constant.MakeFromLiteral("0", token.INT, 0)),
+		"IMAGE_FILE_MACHINE_WCEMIPSV2":         reflect.ValueOf(constant.MakeFromLiteral("361", token.INT, 0)),
+		"NewFile":                              reflect.ValueOf(pe.NewFile),
+		"Open":                                 reflect.ValueOf(pe.Open),
+
+		// type definitions
+		"COFFSymbol":       reflect.ValueOf((*pe.COFFSymbol)(nil)),
+		"DataDirectory":    reflect.ValueOf((*pe.DataDirectory)(nil)),
+		"File":             reflect.ValueOf((*pe.File)(nil)),
+		"FileHeader":       reflect.ValueOf((*pe.FileHeader)(nil)),
+		"FormatError":      reflect.ValueOf((*pe.FormatError)(nil)),
+		"ImportDirectory":  reflect.ValueOf((*pe.ImportDirectory)(nil)),
+		"OptionalHeader32": reflect.ValueOf((*pe.OptionalHeader32)(nil)),
+		"OptionalHeader64": reflect.ValueOf((*pe.OptionalHeader64)(nil)),
+		"Reloc":            reflect.ValueOf((*pe.Reloc)(nil)),
+		"Section":          reflect.ValueOf((*pe.Section)(nil)),
+		"SectionHeader":    reflect.ValueOf((*pe.SectionHeader)(nil)),
+		"SectionHeader32":  reflect.ValueOf((*pe.SectionHeader32)(nil)),
+		"StringTable":      reflect.ValueOf((*pe.StringTable)(nil)),
+		"Symbol":           reflect.ValueOf((*pe.Symbol)(nil)),
+	}
+}
+
+func init_debug_plan9obj() {
+	Symbols["debug/plan9obj"] = map[string]reflect.Value{
+		// function, constant and variable definitions
+		"Magic386":   reflect.ValueOf(constant.MakeFromLiteral("491", token.INT, 0)),
+		"Magic64":    reflect.ValueOf(constant.MakeFromLiteral("32768", token.INT, 0)),
+		"MagicAMD64": reflect.ValueOf(constant.MakeFromLiteral("35479", token.INT, 0)),
+		"MagicARM":   reflect.ValueOf(constant.MakeFromLiteral("1607", token.INT, 0)),
+		"NewFile":    reflect.ValueOf(plan9obj.NewFile),
+		"Open":       reflect.ValueOf(plan9obj.Open),
+
+		// type definitions
+		"File":          reflect.ValueOf((*plan9obj.File)(nil)),
+		"FileHeader":    reflect.ValueOf((*plan9obj.FileHeader)(nil)),
+		"Section":       reflect.ValueOf((*plan9obj.Section)(nil)),
+		"SectionHeader": reflect.ValueOf((*plan9obj.SectionHeader)(nil)),
+		"Sym":           reflect.ValueOf((*plan9obj.Sym)(nil)),
 	}
 }
 
